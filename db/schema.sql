b3de84c6fb68568ca7920e4e8809312573dcae25
Jacob Fugal <lukfugl@gmail.com>
2009-01-25 23:48:28 -0700

    added some more indices

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,11 @@
 CREATE TABLE "slots" (
+	"id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	"day" date,
 	"hour" integer,
 	"layer" varchar(255),
 	"label" varchar(255)
 );
 CREATE UNIQUE INDEX "slots_primary_key" ON "slots" ("day", "hour", "layer");
-CREATE INDEX "slots_without_layer" ON "slots" ("day", "hour");
+CREATE INDEX "slots_index_day_hour" ON "slots" ("day", "hour");
+CREATE INDEX "slots_index_day_layer" ON "slots" ("day", "layer");
+CREATE INDEX "slots_index_day" ON "slots" ("day");

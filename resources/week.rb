fa628dbefea06370edd520b0b4939005459c4dbf
Jacob Fugal <lukfugl@gmail.com>
2009-02-03 00:29:55 -0700

    basic (*very* basic) pdf capability

--- resources/week.rb
+++ resources/week.rb
@@ -1,5 +1,7 @@
 require 'models/slot'
 require 'date'
+require 'prawn'
+require 'prawn/layout'
 
 helpers do
   def parse_date(params)
@@ -49,8 +51,49 @@ get '/week/:date?' do
   }
 end
 
-# it would be nice to make this an alias instead of a redirect, so they can
-# bookmark it. how do we do that in sinatra?
-get '/week/?' do
-  redirect(Time.now.strftime("/week/%Y-%m-%d"))
+get '/week/:date/pdf' do
+  # load the dates and make sure we've redirected to the canonical date for the
+  # week (sunday), except in the case of no-date-provided
+  pass unless parse_date(params)
+
+  # load the labels from the database
+  load_days
+
+  doc = Prawn::Document.new(:page_layout => :landscape) do |pdf|
+    header = @sunday.strftime(@sunday.year == @saturday.year ? '%B %d' : '%B %d, %Y') +
+      "—" + @saturday.strftime('%B %d, %Y')
+
+    data = (8..20).map do |hour|
+      [
+        "%02d00" % hour,
+        *@days.map do |day|
+          slot = day[:slots].detect{ |s| s.hour == hour }
+          slot ? slot.label : ''
+        end
+      ]
+    end
+
+    pdf.header(pdf.margin_box.top_left) do
+      pdf.text header
+      pdf.stroke_horizontal_rule
+    end
+
+    headers = [ '', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
+    column_width = pdf.margin_box.width / 9
+    column_widths = {}
+    (0..7).each{ |i| column_widths[i] = column_width }
+
+    pdf.pad_top(50) do
+      pdf.table data,
+        :position => :center,
+        :headers => headers,
+        :row_colors => [ 'ffffff', 'dddddd' ],
+        :column_widths => column_widths,
+        :align => { 0 => :right }
+    end
+  end
+
+  content_type 'application/pdf'
+  attachment "riker-#{@sunday}.pdf"
+  doc.render
 end
